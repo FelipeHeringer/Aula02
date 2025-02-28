e098acc176dd68c9012074ec11debabaf054ae3a
Felipe Heringer <heringerfelipe14@gmail.com>
2025-02-27 23:01:41 -0300

    Branch Main metodo Hello World

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+package Aula02.src;
+
+public class Hello {
+    public void helloWorld(){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+package Aula02.src;
+
+public class Main {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.helloWorld();
+    }
+}
