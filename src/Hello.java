e578f9a5cfda84e985fabca6745edbf921e76dba
Felipe Heringer <heringerfelipe14@gmail.com>
2025-02-27 23:08:46 -0300

    Branch holaQueTal metodo holaQueTal adicionado

--- src/Hello.java
+++ src/Hello.java
@@ -4,4 +4,7 @@ public class Hello {
     public void helloWorld(){
         System.out.println("Hello World!");
     }
+    public void holaQueTal(){
+        System.out.println("Hola que Tal!");
+    }
 }
