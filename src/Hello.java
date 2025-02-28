9171c59ddf606b470702e6f7bf0ce2b4f4b3d674
Felipe Heringer <heringerfelipe14@gmail.com>
2025-02-27 23:04:48 -0300

    Branch olaMundo metodo olaMundo adicionado

--- src/Hello.java
+++ src/Hello.java
@@ -4,4 +4,7 @@ public class Hello {
     public void helloWorld(){
         System.out.println("Hello World!");
     }
+    public void olaMundo(){
+        System.out.println("Ola Mundo !");
+    }
 }
